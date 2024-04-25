eb201e46da0cacd5b6540f8a89ec7d4cd55a1fdd
Eric Jang <ericjang@Erics-MacBook-Pro.local>
2024-04-25 14:41:00 -0700

    next app config

---
+++ dev-portfolio/next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    basePath: "/ericj111.github.io",
+    images: {
+        unoptimized: true
+    }
+
+};
+
+export default nextConfig;
