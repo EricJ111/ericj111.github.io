777131b53b1a6f1bd7d60d81b2126b29fd66482e
Eric Jang <ericjang@Erics-MacBook-Pro.local>
2024-04-25 15:29:53 -0700

    basePath as env var

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
+
+export default (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      /* development only config options here */
+    };
+  }
+  return {
+    /* config options for all phases except development here */
     output: "export",
     basePath: "/ericj111.github.io",
     images: {
-        unoptimized: true
-    }
+      unoptimized: true,
+    },
+  };
 };
-
-export default nextConfig;
