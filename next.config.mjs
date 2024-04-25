516d75a1e8a898ab95e9872c0e2dbda3db08c8b2
Eric Jang <ericjang@Erics-MacBook-Pro.local>
2024-04-25 14:54:20 -0700

    fix nextConfig basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "ericj111.github.io",
+    basePath: "/ericj111.github.io",
     images: {
         unoptimized: true
     }
