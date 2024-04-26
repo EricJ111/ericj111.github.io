d17a966121fb9ac20881da99e80faa5e8870de6f
Eric Jang <ericjang@Erics-MacBook-Pro.local>
2024-04-26 14:35:42 -0700

    add nojekyll to css

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,6 @@ export default (phase, { defaultConfig }) => {
     /* config options for all phases except development here */
     output: "export",
     basePath: "/ericj111.github.io",
-    assetPrefix: "/ericj111.github.io/",
     images: {
       unoptimized: true,
     },
